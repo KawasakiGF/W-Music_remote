6bf1385970cc6751c778e5aa0fb4dac8c64e5ddd
yuyujan <yu.main.address2@gmail.com>
2022-02-19 03:29:00 +0900

    metadataを全部回収できるように LogcatにてDebugレベルで"Realm_ADD"を検索

--- app/src/main/java/com/example/music/MainActivity.java
+++ app/src/main/java/com/example/music/MainActivity.java
@@ -2,9 +2,17 @@ package com.example.music;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.content.res.AssetManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Menu;
 
@@ -20,32 +28,29 @@ import com.example.music.databinding.ActivityMainBinding;
 import com.google.android.material.navigation.NavigationView;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 import io.realm.Realm;
+import io.realm.RealmAsyncTask;
+import io.realm.RealmResults;
 
 //importしろとは言われてなかったけど、動かないから多少はね？
 //import com.example.music.MediaMetaRetriever;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final int PERMISSION_WRITE_EX_STR = 1;
     Realm realm;
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
-    private static final int PERMISSION_WRITE_EX_STR = 1;
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-//        if (transaction != null && !transaction.isCancelled()) {
-//            transaction.cancel();
-//        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        realm=Realm.getDefaultInstance();
+        realm = Realm.getDefaultInstance();
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -63,8 +68,6 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
 
-        /* assets/musicFolder内のファイルをログ表示.*/
-        displayAssets("musicFolder");
 
         int permission = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
         //ストレージへの読み込み許可を取る
@@ -81,34 +84,26 @@ public class MainActivity extends AppCompatActivity {
         }
         if (permission == PackageManager.PERMISSION_GRANTED) {
             //ここでストレージ読み込み処理を呼びたい
-  //          ModelUpdater(realm);
-        }
-
-
-    }
 /*
-    @SuppressLint("Range")
-    public void ModelUpdater(Realm realm) {
 
+            ContentResolver contentResolver = getContentResolver();
+            Cursor cursor = null;
+            StringBuilder sb = null;
+            Log.d("ModelUpdater", "ModelUpdater wad called. ");
 
+            try {
+                cursor = contentResolver.query(
+                        MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                        null, null, null, null);
 
-
-
-        final String TAG = "ModelUpdater";
-        ContentResolver contentResolver = getContentResolver();
-        Cursor cursor = null;
-        StringBuilder sb = null;
-        Log.d(TAG, "ModelUpdater wad called. ");
-
-        try {
-            cursor = contentResolver.query(
-                    MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
-                    null, null, null, null);
-
-            if (cursor != null && cursor.moveToFirst()) {
-                do {*/
-
-/*
+                String str = String.format(
+                        "MediaStore.Audio = %s\n\n", cursor.getCount());
+                Log.d("getcount","曲数: "+str);
+                int aaa =Integer.parseInt(str);
+                int [aaa][7] datalist;
+                if (cursor != null && cursor.moveToFirst()) {
+                    do {
+*//*
                     //以下のコメントアウトは参考用に.append(str型のsbに文字を追加処理)のプログラムを残す
                     sb.append("ID: ");
                     sb.append(cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media._ID)));
@@ -121,47 +116,88 @@ public class MainActivity extends AppCompatActivity {
                     sb.append(cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATA)));
                     Log.d("WATCH_METADATA", cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATA)));
                     sb.append("\n\n");
-*/
+*//*
 
-/*
-                } while (cursor.moveToNext());
+                    } while (cursor.moveToNext());
 
-                cursor.close();
+                    cursor.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+
+                Toast toast = Toast.makeText(this,
+                        "例外が発生、Permissionを許可していますか？", Toast.LENGTH_SHORT);
+                toast.show();
+
+                //MainActivityに戻す
+                finish();
+            } finally {
+                if (cursor != null) {
+                    cursor.close();
+                }
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+            */
+            List<MusicItem> Items = MusicItem.getItems(getApplicationContext());
+            RealmAsyncTask transaction = realm.executeTransactionAsync(new Realm.Transaction() {
+                @Override
+                public void execute(Realm bgRealm) {
+                    Log.d("Realm_ADD", "execute begin");
+                    //                   MusicMetaDataModel DataModel = bgRealm.createObject(MusicMetaDataModel.class);
 
-            Toast toast = Toast.makeText(this,
-                    "例外が発生、Permissionを許可していますか？", Toast.LENGTH_SHORT);
-            toast.show();
+                    int i = 0;
+                    for (i = 0; i < Items.size(); i++) {
+                        RealmResults<MusicMetaDataModel> finds = bgRealm.where(MusicMetaDataModel.class).equalTo("path", Items.get(i).path).findAll();
+                        if (finds.size() > 0) {//更新処理
 
-            //MainActivityに戻す
-            finish();
-        } finally {
-            if (cursor != null) {
-                cursor.close();
-            }
+                            finds.get(0).artist=Items.get(i).artist;
+                            finds.get(0).truck=Items.get(i).truck;
+                            finds.get(0).title=Items.get(i).title;
+                            finds.get(0).album=Items.get(i).album;
+                            finds.get(0).id=Items.get(i).id;
+                            finds.get(0).duration=Items.get(i).duration;
+
+
+                        } else {//追加処理
+
+                            MusicMetaDataModel metadata = bgRealm.createObject(MusicMetaDataModel.class,Items.get(i).path);
+
+
+                            metadata.artist=Items.get(i).artist;
+                            metadata.truck=Items.get(i).truck;
+                            metadata.title=Items.get(i).title;
+                            metadata.album=Items.get(i).album;
+                            metadata.id=Items.get(i).id;
+                            metadata.duration=Items.get(i).duration;
+                            bgRealm.copyFromRealm(metadata);
+
+                        }
+                    }
+                    Log.d("Realm_ADD", String.valueOf(i)+" musics are collected");
+                }
+            }, new Realm.Transaction.OnSuccess() {
+                @Override
+                public void onSuccess() {
+                    // トランザクションは成功
+                    Log.d("Realm_ADD", "onSuccess: ");
+                }
+            }, new Realm.Transaction.OnError() {
+                @Override
+                public void onError(Throwable error) {
+                    // トランザクションは失敗。自動的にキャンセルされます
+                    Log.d("Realm_ADD", "onError: ");
+                }
+            });
+
         }
 
-        realm.executeTransactionAsync(new Realm.Transaction() {
-            @Override
-            public void execute(Realm bgRealm) {
-                MusicMetaDataModel DataModel = bgRealm.createObject(MusicMetaDataModel.class);
-                RealmResults<MusicMetaDataModel> finds= realm.where(MusicMetaDataModel.class).equalTo("path",cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATA))).findAll();
-            } //todo cursorの受け渡しが無理っぽいのでArrayにデータを纏めてからRealm送りにするか‥
-        }, new Realm.Transaction.OnSuccess() {
-            @Override
-            public void onSuccess() {
-                // トランザクションは成功
-            }
-        }, new Realm.Transaction.OnError() {
-            @Override
-            public void onError(Throwable error) {
-                // トランザクションは失敗。自動的にキャンセルされます
-            }
-        });
+
     }
-*/
+
+    /*
+        @SuppressLint("Range")
+        public void ModelUpdater(Realm realm) {
+        }
+    */
     //許可が取れているかの確認
     @SuppressLint("Range")
     @Override
@@ -176,6 +212,61 @@ public class MainActivity extends AppCompatActivity {
                 if (grantResults[0] == android.content.pm.PackageManager.PERMISSION_GRANTED) {
                     /// 許可が取れた場合・・・
                     /// 必要な処理を書いておく
+                    List<MusicItem> Items = MusicItem.getItems(getApplicationContext());
+                    RealmAsyncTask transaction = realm.executeTransactionAsync(new Realm.Transaction() {
+                        @Override
+                        public void execute(Realm bgRealm) {
+                            Log.d("Realm_ADD", "execute begin");
+                            //                   MusicMetaDataModel DataModel = bgRealm.createObject(MusicMetaDataModel.class);
+
+                            int i = 0;
+                            for (i = 0; i < Items.size(); i++) {
+
+                                Log.d("Realm_ADD", "RealmResults begin");
+                                RealmResults<MusicMetaDataModel> finds = bgRealm.where(MusicMetaDataModel.class).equalTo("path", Items.get(i).path).findAll();
+                                Log.d("Realm_ADD", "RealmResults done");
+                                if (finds.size() > 0) {//更新処理
+
+                                    Log.d("Realm_ADD", "if");
+                                    finds.get(0).artist=Items.get(i).artist;
+                                    finds.get(0).truck=Items.get(i).truck;
+                                    finds.get(0).title=Items.get(i).title;
+                                    finds.get(0).album=Items.get(i).album;
+                                    finds.get(0).id=Items.get(i).id;
+                                    finds.get(0).duration=Items.get(i).duration;
+
+
+                                    Log.d("Realm_ADD", "set Album-Duration done.");
+                                } else {//追加処理
+
+                                    MusicMetaDataModel metadata = bgRealm.createObject(MusicMetaDataModel.class,Items.get(i).path);
+
+
+                                    metadata.artist=Items.get(i).artist;
+                                    metadata.truck=Items.get(i).truck;
+                                    metadata.title=Items.get(i).title;
+                                    metadata.album=Items.get(i).album;
+                                    metadata.id=Items.get(i).id;
+                                    metadata.duration=Items.get(i).duration;
+                                    bgRealm.copyFromRealm(metadata);
+
+                                }
+                            }
+                        }
+                    }, new Realm.Transaction.OnSuccess() {
+                        @Override
+                        public void onSuccess() {
+                            // トランザクションは成功
+                            Log.d("Realm_ADD", "onSuccess: ");
+                        }
+                    }, new Realm.Transaction.OnError() {
+                        @Override
+                        public void onError(Throwable error) {
+                            // トランザクションは失敗。自動的にキャンセルされます
+                            Log.d("Realm_ADD", "onError: ");
+                        }
+                    });
+
                 } else {
                     /// 許可が取れなかった場合・・・
                     /// catchでこの処理を描いたので要らないかもしれん
@@ -205,7 +296,7 @@ public class MainActivity extends AppCompatActivity {
     private void displayAssets(String dir) {
         AssetManager assetMgr = getResources().getAssets();
         try {
-            String musicList[] = assetMgr.list(dir);
+            String[] musicList = assetMgr.list(dir);
             for (int i = 0; i < musicList.length; i++) {
                 Log.d("assets file", musicList[i]);
             }
@@ -213,4 +304,123 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+//        if (transaction != null && !transaction.isCancelled()) {
+//            transaction.cancel();
+//        }
+    }
+
+    public static class MusicItem implements Comparable<Object> {
+        private static final String TAG = "MusicItem";
+        final long id;
+        final String artist;
+        final String title;
+        final String album;
+        final int truck;
+        final long duration;
+        final String path;
+
+        public MusicItem(long id, String artist, String title, String album, int truck, long duration, String path) {
+            this.id = id;
+            this.artist = artist;
+            this.title = title;
+            this.album = album;
+            this.truck = truck;
+            this.duration = duration;
+            this.path = path;
+        }
+
+        /**
+         * 外部ストレージ上から音楽を探してリストを返す。
+         *
+         * @param context コンテキスト
+         * @return 見つかった音楽のリスト
+         */
+        public static List<MusicItem> getItems(Context context) {
+            List<MusicItem> items = new LinkedList<MusicItem>();
+
+            // ContentResolver を取得
+            ContentResolver cr = context.getContentResolver();
+
+            // 外部ストレージから音楽を検索
+            Cursor cur = cr.query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, null, MediaStore.Audio.Media.IS_MUSIC + " = 1", null, null);
+
+            if (cur != null) {
+                if (cur.moveToFirst()) {
+                    Log.i(TAG, "Listing...");
+
+                    // 曲情報のカラムを取得
+                    int artistColumn = cur.getColumnIndex(MediaStore.Audio.Media.ARTIST);
+                    int titleColumn = cur.getColumnIndex(MediaStore.Audio.Media.TITLE);
+                    int albumColumn = cur.getColumnIndex(MediaStore.Audio.Media.ALBUM);
+                    int durationColumn = cur.getColumnIndex(MediaStore.Audio.Media.DURATION);
+                    int idColumn = cur.getColumnIndex(MediaStore.Audio.Media._ID);
+                    int idTruck = cur.getColumnIndex(MediaStore.Audio.Media.TRACK);
+                    int dataColumn = cur.getColumnIndex(MediaStore.Audio.AudioColumns.DATA);
+
+                    Log.i(TAG, "Title column index: " + titleColumn);
+                    Log.i(TAG, "ID column index: " + titleColumn);
+
+                    // リストに追加。>>item<< に曲の全てのデータが入ってる
+                    do {
+                        Log.i(TAG, "ID: " + cur.getString(idColumn) + " Title: " + cur.getString(titleColumn));
+                        items.add(new MusicItem(cur.getLong(idColumn),
+                                cur.getString(artistColumn),
+                                cur.getString(titleColumn),
+                                cur.getString(albumColumn),
+                                cur.getInt(idTruck),
+                                cur.getLong(durationColumn),
+                                cur.getString(dataColumn)));
+                    } while (cur.moveToNext());
+                    Log.i(TAG, "Done querying media. MusicRetriever is ready.");
+                }
+                // カーソルを閉じる
+                cur.close();
+            }
+
+            // 見つかる順番はソートされていないため、アルバム単位でソートする
+            Collections.sort(items);
+            return items;
+        }
+
+        public Uri getURI() {
+            return ContentUris.withAppendedId(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, id);
+        }
+
+        @Override
+        public int compareTo(Object another) {
+            if (another == null) {
+                return 1;
+            }
+
+            MusicItem item = (MusicItem) another;
+            int result = album.compareTo(item.album);
+
+            if (result != 0) {
+                return result;
+            }
+
+            return truck - item.truck;
+        }
+
+        public class ArtWork {
+            public Bitmap getArtWork(String filePath) {
+
+                Bitmap bm = null; //bmに画像ファイルが入る
+
+                MediaMetadataRetriever mmr = new MediaMetadataRetriever();
+                mmr.setDataSource(filePath);
+
+                byte[] data = mmr.getEmbeddedPicture();
+
+                // 画像が無ければnullになる
+                if (null != data) {
+                    bm = BitmapFactory.decodeByteArray(data, 0, data.length);
+                }
+                return bm;
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/music/MusicMetaDataModel.java
+++ app/src/main/java/com/example/music/MusicMetaDataModel.java
@@ -5,49 +5,19 @@ import io.realm.annotations.PrimaryKey;
 
 public class MusicMetaDataModel extends RealmObject {
 
-    private long id;
-    private String artist;
-    private String title;
-    private String album;
-    private int truck;
-    private long duration;
+    public long id;
+    public String artist;
+    public String title;
+    public String album;
+    public int truck;
+    public long duration;
 
     @PrimaryKey
-    private String path;
+    public String path;
 
-    public void setPath(String path) {
-        this.path = path;
+
     }
 
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public void setArtist(String artist) {
-        this.artist = artist;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setAlbum(String album) {
-        this.album = album;
-    }
-
-    public void setTruck(int truck) {this.truck = truck;}
-
-    public void setDuration(long duration) {this.duration = duration;}
-
-    public long getId(){return id;}
-    public String getArtist(){return artist;}
-    public String  getTitle(){return title;}
-    public String  getAlbum(){return album;}
-    public int getTruck(){return truck;}
-    public long getDuration(){return duration;}
-    public String getPath(){return path;
-    }
-}
 /*
 楽曲のメタデータを保存するためのモデルを定義
 MainActivity.java内のpublic MusicItem()の定義を参考にしてidからpathまで受け取れるようにしてある
