77eac9b58733b5690043e8602a9020a4156dc7f7
yuyujan <yu.main.address2@gmail.com>
2022-02-20 18:02:50 +0900

    mediaPlayer = MediaPlayer.create(getContext(), uri);までは動作するように

--- app/src/main/java/com/example/music/ui/sound/SoundFragment.java
+++ app/src/main/java/com/example/music/ui/sound/SoundFragment.java
@@ -1,67 +1,57 @@
 package com.example.music.ui.sound;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.ContentUris;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
-import android.util.Log;
-import android.content.res.AssetFileDescriptor;
+import android.widget.Chronometer;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
-import android.widget.Toast;
-import java.io.IOException;
-import java.util.zip.Inflater;
 
-import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.SeekBar;
-import android.widget.Chronometer;
-import androidx.core.content.ContextCompat;
 
-import com.example.music.R;
-import com.example.music.databinding.FragmentGalleryBinding;
-import com.example.music.databinding.FragmentSoundBinding;
 import com.example.music.MusicMetaDataModel;
-import com.example.music.ui.gallery.GalleryViewModel;
+import com.example.music.R;
+import com.example.music.databinding.FragmentSoundBinding;
 
 import io.realm.Realm;
 import io.realm.RealmResults;
 
 public class SoundFragment extends Fragment {
 //public class SoundFragment extends AppCompatActivity {
-
+String TAG="SoundFragment_LOG";
     public MusicMetaDataModel metaData;
     public MediaPlayer mediaPlayer;
-    private SoundViewModel soundViewModel;
     public FragmentSoundBinding binding;
     public SeekBar seekBar;
     public Chronometer chronometer;
     public Chronometer durationMax;
     public ImageView button;
     public ImageView jacket;
-
     //public String path;
-    Realm realm=Realm.getDefaultInstance();
+    Realm realm = Realm.getDefaultInstance();
     //RealmResults<MusicMetaDataModel> wantedMusic = realm.where(MusicMetaDataModel.class).equalTo("path",path).findAll();
     //long duration=wantedMusic.first().duration;
     RealmResults<MusicMetaDataModel> allList = realm.where(MusicMetaDataModel.class).findAll();
     String path = allList.first().path;
+    Uri uri = ContentUris.withAppendedId(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,allList.first().id);
+    long duration = allList.first().duration;
+    private SoundViewModel soundViewModel;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_sound, container, false);
+        Log.d(TAG, "onCreateView: done");
 
         //曲名表示
         soundViewModel = new ViewModelProvider(this).get(SoundViewModel.class);
@@ -76,12 +66,14 @@ public class SoundFragment extends Fragment {
             }
         });
 
-        ImageView button = (ImageView)view.findViewById(R.id.playButton);
+        ImageView button = (ImageView) view.findViewById(R.id.playButton);
         super.onCreate(savedInstanceState);
         //ここのRを通すために26行目にRのimportを入れてる。いや、下のは不要かも
         //setContentView(R.layout.fragment_sound);
         mediaPlayer = null;
-        mediaPlayer = MediaPlayer.create(this.getApplication(),path);
+        mediaPlayer = MediaPlayer.create(getContext(), uri);
+        Log.d(TAG, "onCreateView: uri= "+uri);
+        Log.d(TAG, "onCreateView: mediaPlayer init");
         //↑合ってると思うけど、曲名を入れてダメやったらあれやから下に元のプログラムを張っておく。
         //mediaPlayer = MediaPlayer.create(getApplicationContext(),R.raw.sound);
 
@@ -100,31 +92,34 @@ public class SoundFragment extends Fragment {
         //ジャケットの画像があれば割り当てる
         //ImageView jacket = (ImageView)view.findViewById(R.id.jacket);
         //jacket = <<<引っ張ってきたjacketをいれる>>>;
+        Log.d(TAG, "onCreateView: end");
         return inflater.inflate(R.layout.fragment_sound, container, false);
     }
 
-    public void onPlayButton(View v){
+    public void onPlayButton(View v) {
         //表示内容の切り替え
         //ImageView button = (ImageView)findViewById(R.id.playButton);
         //再生中なら停止、停止中なら再生
-        if(mediaPlayer.isPlaying()){
+        if (mediaPlayer.isPlaying()) {
             mediaPlayer.pause();
+            Log.d(TAG, "onPlayButton: pause() called");
             button.setImageResource(R.drawable.start);
             //曲の進行時間をストップ
             chronometer.stop();
-        }
-        else{
+        } else {
             mediaPlayer.start();
+            Log.d(TAG, "onPlayButton: start() called");
             button.setImageResource(R.drawable.pause);
             //曲の進行時間を表示
             chronometer.setBase(SystemClock.elapsedRealtime());
             chronometer.start();
         }
     }
+
     //seekbarを離した時
     public void onStopTrackingTouch(SeekBar seekBar) {
         //seekのみ行う
-        int msec = (int)duration * seekBar.getProgress() / seekBar.getMax();
+        int msec = (int) duration * seekBar.getProgress() / seekBar.getMax();
         mediaPlayer.seekTo(msec);
         chronometer.setBase(SystemClock.elapsedRealtime() - mediaPlayer.getCurrentPosition());
 
@@ -134,7 +129,7 @@ public class SoundFragment extends Fragment {
     public void onStartTrackingTouch(SeekBar seekBar) {
         //すべて一時停止する
         chronometer.stop();
-        if(mediaPlayer.isPlaying()) {
+        if (mediaPlayer.isPlaying()) {
             mediaPlayer.pause();
         }
         button.setImageResource(R.drawable.pause);
@@ -147,94 +142,92 @@ public class SoundFragment extends Fragment {
     @Override
     public void onDestroy() {
         super.onDestroy();
+        Log.d(TAG, "onDestroy: Destroy");
         binding = null;
         mediaPlayer.release();
         mediaPlayer = null;
     }
+
     @Override
     public void onPause() {
         super.onPause();
     }
+
     @Override
     public void onResume() {
         super.onResume();
     }
 
 
-        // 音楽開始ボタン
-        //Button buttonPlay = findViewById(R.id.play);
-        //buttonPlay.setOnClickListener(v -> {
-        //    audioPlay();
-        //});
+    // 音楽開始ボタン
+    //Button buttonPlay = findViewById(R.id.play);
+    //buttonPlay.setOnClickListener(v -> {
+    //    audioPlay();
+    //});
 
-        // 音楽停止ボタン
-        //Button buttonStop = findViewById(R.id.stop);
+    // 音楽停止ボタン
+    //Button buttonStop = findViewById(R.id.stop);
 
-        //buttonStop.setOnClickListener( v -> {
-        //    if (mediaPlayer != null) {
-        //        audioStop();
-        //    }
-        //});
+    //buttonStop.setOnClickListener( v -> {
+    //    if (mediaPlayer != null) {
+    //        audioStop();
+    //    }
+    //});
     //}
     //private void audioPlay(){
-        //if(mediaPlayer == null){
-            //audioファイルを読み出し
-        //}
-        //else{
-            // 繰り返し再生する場合
-        //    mediaPlayer.stop();
-        //    mediaPlayer.reset();
-            // リソースの解放
-        //    mediaPlayer.release();
-        //}
-        // 再生する
-        //mediaPlayer.start();
+    //if(mediaPlayer == null){
+    //audioファイルを読み出し
+    //}
+    //else{
+    // 繰り返し再生する場合
+    //    mediaPlayer.stop();
+    //    mediaPlayer.reset();
+    // リソースの解放
+    //    mediaPlayer.release();
+    //}
+    // 再生する
+    //mediaPlayer.start();
     //}
 
     //private void audioStop() {
-        // 再生終了
-        //mediaPlayer.stop();
-        // リセット
-        //mediaPlayer.reset();
-        // リソースの解放
-        //mediaPlayer.release();
+    // 再生終了
+    //mediaPlayer.stop();
+    // リセット
+    //mediaPlayer.reset();
+    // リソースの解放
+    //mediaPlayer.release();
 
-        //mediaPlayer = null;
+    //mediaPlayer = null;
     //}
-
-
-
-
 
 
     //public View onCreateView(@NonNull LayoutInflater inflater,
     //                         ViewGroup container, Bundle savedInstanceState) {
-        //setContentView(R.layout.fragment_sound);
-        //soundViewModel =
-        //        new ViewModelProvider(this).get(SoundViewModel.class);
+    //setContentView(R.layout.fragment_sound);
+    //soundViewModel =
+    //        new ViewModelProvider(this).get(SoundViewModel.class);
 
 
-        //AssetManager assetMgr = getResources().getAssets();
-        //try {
-        //    String[] musicList = assetMgr.list("musicFolder");
-        //} catch (IOException e) {
-        //}
+    //AssetManager assetMgr = getResources().getAssets();
+    //try {
+    //    String[] musicList = assetMgr.list("musicFolder");
+    //} catch (IOException e) {
+    //}
 
-        //binding = FragmentSoundBinding.inflate(inflater, container, false);
-        //View root = binding.getRoot();
-
-
-
-        //final TextView textView = binding.textSound;
-        //soundViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            //@Override
-            //public void onChanged(@Nullable String s) {
-                //textView.setText(s);
+    //binding = FragmentSoundBinding.inflate(inflater, container, false);
+    //View root = binding.getRoot();
 
 
-            //}
-        //});
-        //return root;
+    //final TextView textView = binding.textSound;
+    //soundViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+    //@Override
+    //public void onChanged(@Nullable String s) {
+    //textView.setText(s);
+
+
+    //}
+    //});
+    //return root;
     //}
 
 }
--- app/src/main/java/com/example/music/ui/sound/SoundViewModel.java
+++ app/src/main/java/com/example/music/ui/sound/SoundViewModel.java
@@ -1,27 +1,21 @@
 package com.example.music.ui.sound;
 
-import android.media.MediaPlayer;
-import android.os.Bundle;
-import android.widget.Button;
-
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-import com.example.music.R;
-import com.example.music.databinding.FragmentSoundBinding;
+import com.example.music.MusicMetaDataModel;
+
 import io.realm.Realm;
 import io.realm.RealmResults;
-import com.example.music.MusicMetaDataModel;
 
 public class SoundViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public SoundViewModel(String path) {
+    public SoundViewModel() {
         Realm realm=Realm.getDefaultInstance();
-        RealmResults<MusicMetaDataModel> wantedMusic = realm.where(MusicMetaDataModel.class).equalTo("path",path).findAll();
+        RealmResults<MusicMetaDataModel> wantedMusic = realm.where(MusicMetaDataModel.class).findAll();
         String title=wantedMusic.first().title;//pathは重複しない（1つのみ）ので、検索結果の一番目の(first)データのtitleを代入する
         mText = new MutableLiveData<>();
         mText.setValue(title);
